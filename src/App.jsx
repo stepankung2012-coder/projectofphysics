00fcec4fab1400407d351cec7cec3016cb0758ec
Stepan and Yulia <stepandyuliarozkovy@MacBook-Air-Stepan-2.local>
2026-08-08 14:48:00 +0300

    Update project diary title

--- src/App.jsx
+++ src/App.jsx
@@ -307,7 +307,7 @@ function App() {
             <Sparkles size={18} />
           </BrandMark>
           <div>
-            <ProductName>Проект по физике</ProductName>
+            <ProductName>Дневник проекта по физике</ProductName>
             <ProductHint>Дневник проектной деятельности</ProductHint>
           </div>
         </Brand>
